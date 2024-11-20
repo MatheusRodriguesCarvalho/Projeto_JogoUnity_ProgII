ab59cff67951dfafe833622a88f778c894d57049
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-11-20 16:05:22 -0300

    update

--- Asteroids/Assets/Script/AsteriodSpawner.cs
+++ Asteroids/Assets/Script/AsteriodSpawner.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class AsteriodSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float spawnTime = 2;
+    public GameObject Asteriod;
     void Start()
     {
-        
+        InvokeRepeating("spawnAsteriods", spawnTime, spawnTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    void spawnAsteriods()
+    {
+        Renderer render = GetComponent<Renderer>();
+        var x1 = transform.position.x - render.bounds.size.x / 2;
+        var x2 = transform.position.x + render.bounds.size.x / 2;
+        var spawnPoint = new Vector2(Random.Range(x1, x2), transform.position.y);
+
+        Instantiate(Asteriod, spawnPoint, Quaternion.identity);
+    }
+
 }
